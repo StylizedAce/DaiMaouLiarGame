28af67a835df1304270b741d391d68cccaf25639
YousefSKoshak <s444000774@st.uqu.edu.sa>
2025-10-06 20:27:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def index():
 
 # Application entry point
 if __name__ == "__main__":
-    DEVELOPMENT = True
+    DEVELOPMENT = False
     if not DEVELOPMENT:
         port = int(os.environ.get("PORT", 5000))
         socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
